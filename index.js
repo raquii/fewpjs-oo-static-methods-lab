cf5909537f9ad59ff97f25b74bcb2fda261b33dd
raquel <raquelanaroman@gmail.com>
2021-06-09 15:26:43 -0400

    commit tree c2eafe2aa2234cd33e96af9c3723f02334f6159d

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string){
+    const exceptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const arrayedString = string.split(' ')
+    const result = [];
+
+    for(let i = 0; i < arrayedString.length; i++){
+      if(i === 0){
+        result.push(this.capitalize(arrayedString[i]))
+      }else if(exceptWords.includes(arrayedString[i])){
+        result.push(arrayedString[i])
+      }else{
+        result.push(this.capitalize(arrayedString[i]))
+      }
+    }
+    return result.join(' ');
+  }
 }
